2cbbaa674e847cedc069aea7610e09c90a322a44
Oleksii Martseniuk <martsenyk.l@gmail.com>
2024-06-15 18:44:27 +0300

    add exception_handler

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
-from fastapi import FastAPI
+import logging
+import logging.config
+
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 
 from src.api.v1.account.router import router as account_router
 from src.api.v1.auth.router import router as auth_router
+from src.config import LOGGING_CONFIG
+from src.exceptions import BaseApplicationException
+
+logging.config.dictConfig(LOGGING_CONFIG)
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/v1")
 app.include_router(account_router, prefix="/v1")
+
+
+@app.exception_handler(BaseApplicationException)
+async def base_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={"detail": exc.message},
+    )
